83b5ecff27746aba74e41803ec9f7ea74ca518a3
Mitch <Mitchelbunk@gmail.com>
2024-03-04 13:58:42 +0100

    eddited connect.php

--- public/connect.php
+++ public/connect.php
@@ -1,5 +1,6 @@
 <?php
-    $firstName = $_POST['userName'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
