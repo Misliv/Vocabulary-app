8a03a1d863fd63af29c67ffb0eed144b277df1e3
Mitch <mBunk@roc-dev.com>
2024-03-06 13:58:38 +0100

    update to login

--- public/login.php
+++ public/login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+
+
 $errors = array();
 
 $users = array(
@@ -39,10 +41,12 @@ if (isset($_POST['knop'])
 <h1><?php echo $message; ?></h1>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <div class="form-group">
+        <!--login with mail in database-->
         <label for="login">Login:</label>
         <input type="text" name="login" value="" required>
     </div>
     <div class="form-group">
+
         <label for="pwd">Password:</label>
         <input type="password" name="pwd" value="" required>
     </div>
