469e35ec9300046d23c5db62db6b9b314ad357b7
Misliv <mayandmays@hotmail.com>
2024-03-06 13:19:35 +0100

    New file called woordenlijst.php Main page can redirect to woordenlijst.php when you click on "Oefenen"

---
+++ public/woordenlijst.php
@@ -0,0 +1,18 @@
+<?php
+/* to do list
+    - als je een nieuwe folder maakt in de woordenlijst laat dat zien op elke page waar je bij de woordenlijst komt.
+    - voor nu zou het fijn zijn als je de woorden kan zien op deze page.
+    - wordt je geredirect naar een nieuwe page als je op een folder klikt? of blijf je op dezelfde page?
+    - je kan nieuwe woorden toevoegen.
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Woordenlijst</title>
+</head>
+<body>
+woordenlijst hier
+</body>
+</html>
