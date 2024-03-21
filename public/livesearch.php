6ad41799e97b2141cb11b039a0fe5c6cd5b3d9e0
Misliv <may.hoang@student.rocvf.nl>
2024-03-21 12:23:01 +0100

    Added a (broken) search engine
    it unfortunately doesn't work.

--- public/livesearch.php
+++ public/livesearch.php
@@ -6,12 +6,12 @@
     <title>Live Search</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script type="text/javascript" src=""script.js></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../src/libs/style.css">
 </head>
 <body>
 <input type="text" id="search" placeholder="Search">
 <br>
-<b>Ex: </b><i>David, Ricky, Ronaldo, Messi, Watson, Robot</i>
+This is a test page.
 <br>
 <div id="display"></div>
 </body>
