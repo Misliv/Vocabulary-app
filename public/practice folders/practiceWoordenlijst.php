b7aa65e446205da66c5ed4bcdecd542da5fe8147
Misliv <may.hoang@student.rocvf.nl>
2024-03-25 14:09:27 +0100

    Rearranged folders, added new files
    Trying to add a function where you can create your own folders where you can add your own words in.

---
+++ public/practice folders/practiceWoordenlijst.php
@@ -0,0 +1,62 @@
+<?php
+/* to do list
+    - als je een nieuwe folder maakt in de woordenlijst laat dat zien op elke page waar je bij de woordenlijst komt.
+    - voor nu zou het fijn zijn als je de woorden kan zien op deze page zonder folder.
+    - wordt je geredirect naar een nieuwe page als je op een folder klikt? of blijf je op dezelfde page?
+    - je kan nieuwe woorden toevoegen.
+    - woordenlijst van oefenen kan je geen veranderingen aan maken, maar woordenlijst van "Bewerk woordenlijst" zou dat wel moeten kunnen.
+    - Sietske's woordenlijst mag niet veranderd worden.
+*/
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Woordenlijst</title>
+</head>
+<body>
+<h1>2000 meest gebruikte woorden</h1>
+<p><a href="folders.php">Terug naar folders</a></p>
+<?php
+echo "<table style 'border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Woord</th><th>Woordsoort</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+    function beginChildren()
+    {
+        echo "<tr>";
+    }
+    function endChildren()
+    {
+        echo "</tr>" . "\n";
+    }
+}
+
+try {
+    $dbh = new PDO('mysql:host=localhost; dbname=woordenschatapp; port=3306', 'root', '');
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $dbh->prepare("SELECT * FROM woord");
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+</body>
+</html>
